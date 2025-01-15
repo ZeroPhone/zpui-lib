e6147911d91bb1f359e9c8f4b9a6e2a594063b2b
CRImier <crimier@yandex.ru>
2025-01-15 10:55:08 +0100

    updating upload.sh; new version

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 rm -rf dist/*
 nano pyproject.toml
-python -m build
-pip install dist/*.whl
+python3 -m build
+pip install -U dist/*.whl
 python3 -m twine upload dist/*
-python -m build
+python3 -m build
