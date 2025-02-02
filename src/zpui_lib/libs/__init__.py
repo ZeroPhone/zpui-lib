44bba49d1c2dab8f066dc30734ef12ae681ecb40
CRImier <crimier@yandex.ru>
2025-02-02 14:09:24 +0100

    v0.2.2: fixed import error on dbus-less systems

--- src/zpui_lib/libs/__init__.py
+++ src/zpui_lib/libs/__init__.py
@@ -1,7 +1,11 @@
 import zpui_lib.libs.lsusb as lsusb
 import zpui_lib.libs.dkms_debug as dkms_debug
 import zpui_lib.libs.dmesg as dmesg
-import zpui_lib.libs.systemctl as systemctl
+
+try:
+    import zpui_lib.libs.systemctl as systemctl
+except:
+    traceback.print_exc()
 
 import zpui_lib.libs.bugreport as bugreport
 import zpui_lib.libs.hw as hw
