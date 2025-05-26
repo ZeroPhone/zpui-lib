99e12ba6ac45f6bca236c31c9131aecb7c39c0a7
CRImier <crimier@yandex.ru>
2025-05-26 02:52:21 +0200

    helpers/general: get_safe_file_backup_path becomes extension-sensitive, only changes its signature a little bit

--- src/zpui_lib/helpers/general.py
+++ src/zpui_lib/helpers/general.py
@@ -24,24 +24,36 @@ def local_path_gen(_name_):
         return os.path.join(app_path, *path)
     return local_path
 
-def_fmt = "{0}_old{1}"
-
-def get_safe_file_backup_path(dir, fname, new_dir = None, fmt = def_fmt):
+def get_safe_file_backup_path(dir, fname, new_dir = None, word="old"):
     """This function lets you safely backup a user's file that you want to move.
     It does this by adding an integer suffix to the target filename,
     and increments that suffix until it's assured that the move target path does not yet exist,
     as long as necessary. This ensures that, whatever file you move, there's always a backup.
+    This function is extension-sensitive - it will try its best to take good care of the file extension.
 
-    You can pass the filename format string to it, (0:old filename, 1:integer),
-    as well as a new directory for the file to be saved into.
+    You can pass a new directory for the file to be saved into.
     """
     if not new_dir: new_dir = dir
+    def get_fn_ext(num): # default function for files without extension
+        nonlocal name, ext
+        return "{}_{}{}.{}".format(name, word, num, ext)
+    def get_fn(num): # default function for files without extension
+        nonlocal fname
+        return "{}_{}{}".format(fname, word, num)
+    fname_els = fname
+    if '.' in fname: # file has an extension
+        if fname.endswith("tar.gz"):
+            name, ext, ext2 = fname.rsplit('.', 2)
+            ext = ext+'.'+ext2
+        else:
+            name, ext = fname.rsplit('.', 1)
+        get_fn = get_fn_ext
     current_path = os.path.join(dir, fname)
     i = 1
-    new_fname = fmt.format(fname, i)
+    new_fname = get_fn(i)
     while new_fname in os.listdir(new_dir):
         i += 1
-        new_fname = fmt.format(fname, i)
+        new_fname = get_fn(i)
     new_path = os.path.join(new_dir, new_fname)
     return current_path, new_path
 
@@ -95,7 +107,29 @@ class TestGeneralHelpers(unittest.TestCase):
             print(file)
         for i in range(1, 11):
             # check that the files are there
-            path = def_fmt.format(fname, i)
+            path = "{}_old{}".format(fname, i)
+            print(path)
+            assert(path in files)
+            os.remove(os.path.join(test_dir, path))
+
+    def test_gsfbp_ext(self):
+        new_paths = []
+        test_dir = "/tmp/"
+        fname = "zpui_sbf_test_fname.txt"
+        name = "zpui_sbf_test_fname"
+        for i in range(10):
+            # call the function 10 times
+            path = os.path.join(test_dir, fname)
+            with open(path, "w") as f:
+                f.write(str(i))
+            old_path, new_path = get_safe_file_backup_path(test_dir, fname)
+            os.rename(old_path, new_path)
+        files = os.listdir(test_dir)
+        for file in files:
+            print(file)
+        for i in range(1, 11):
+            # check that the files are there
+            path = "{}_old{}.txt".format(name, i)
             print(path)
             assert(path in files)
             os.remove(os.path.join(test_dir, path))
