1a86b50793919b9d52e25cf6a1397ee754056c90
CRImier <crimier@yandex.ru>
2025-06-16 09:09:30 +0200

    /: from ZPUI, moving ui/, apps/zero_app.py, actions.py to zpui_lib; v1.0.0

--- src/zpui_lib/test.sh
+++ src/zpui_lib/test.sh
@@ -2,9 +2,9 @@
 
 set -euxo pipefail
 
-#find ui/tests/ -maxdepth 1 -name '*.pyc' -delete
+find ui/tests/ -maxdepth 1 -name '*.pyc' -delete
 
-#test -d ui/tests/__pycache__ && rm -rf ui/tests/__pycache__
+test -d ui/tests/__pycache__ && rm -rf ui/tests/__pycache__
 #test -d tests/__pycache__ && rm -rf tests/__pycache__
 #test -d .pytest_cache && rm -rf .pytest_cache/
 
