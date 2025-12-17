063ed3a2f7fac1af899a7613cf7764ef010e35b4
CRImier <crimier@yandex.ru>
2025-12-17 06:41:27 +0100

    upload.sh: updating for quicker local iterations

--- upload.sh
+++ upload.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -eux
 
 rm -rf dist/*
-nano pyproject.toml
-git add pyproject.toml
+if [ ${1-default} != "local" ] ; then
+    nano pyproject.toml
+    git add pyproject.toml
+fi
 python3 -m build
 pip install -U --force-reinstall dist/*.whl
 if [ ${1-default} != "local" ] ; then
